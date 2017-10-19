9fd8b31c80a732705a6d0e46fd7860113411ceec
kishimoto.mayumi <kishimoto.mayumi@toyoko-sys.co.jp>
2017-10-20 07:06:52 +0900

    課題２作成完了

---
+++ LearningOutcomes/Theme2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //変数の宣言
+            string a; //入力値
+            
+            Console.Write("整数を入力してください：");
+            a = Console.ReadLine();
+            int inputA = int.Parse(a);
+
+            int Sign = System.Math.Sign(inputA);
+            
+            if (Sign == 1)
+            {
+                Console.WriteLine("その数は正です。");
+            }
+            else if (Sign == -1)
+            {
+                Console.WriteLine("その数は負です。");
+            }
+            else
+            {
+                Console.WriteLine("その数は0です。");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
