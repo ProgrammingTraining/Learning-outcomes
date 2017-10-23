b37ec8707bc296a2c0698728b92bd13a41f869dc
kishimoto.mayumi <kishimoto.mayumi@toyoko-sys.co.jp>
2017-10-23 19:40:29 +0900

    課題4の作成

---
+++ LearningOutcomes/Theme4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int result = 0;
+            for (int i=1; i<=10;i++)
+            {
+                result += i;
+            }
+            Console.WriteLine("1から10までの和は"+ result + "です。");
+            Console.ReadLine();
+        }
+    }
+}
