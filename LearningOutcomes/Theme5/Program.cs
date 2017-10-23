afbe46084447beceaf6e92d8a62b4cf63d8a4a6e
kishimoto.mayumi <kishimoto.mayumi@toyoko-sys.co.jp>
2017-10-24 07:54:26 +0900

    課題5の作成

---
+++ LearningOutcomes/Theme5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int i=1;
+            int result = 0;
+
+            Console.Write("整数を入力してください：");
+            input = Console.ReadLine();
+            int n = int.Parse(input);
+            
+            while (i <= n)
+            {
+                result += i;
+                i++;
+            }
+            Console.WriteLine("1から" + input + "までの和は" + result + "です。");
+            Console.ReadLine();
+        }
+    }
+}
