de9b61b09afb9d82fbcc218262527b798f1707b9
kishimoto.mayumi <kishimoto.mayumi@toyoko-sys.co.jp>
2017-11-06 19:31:10 +0900

    棒グラフ完成

--- AlgorithmPictureBook/AlgorithmPictureBook/BarChart/Program.cs
+++ AlgorithmPictureBook/AlgorithmPictureBook/BarChart/Program.cs
@@ -10,6 +10,21 @@ namespace BarChart
     {
         static void Main(string[] args)
         {
+            int length, i, j;
+
+            int[] a = {4,5,2,3};
+            length = a.Length - 1;
+
+            for(i = 0; i <= length; i++)
+            {
+                Console.Write("a[" + i + "]=" + a[i]+"|");
+                for (j = 1; j <= a[i]; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.ReadLine();
+            }
+            Console.ReadLine();
         }
     }
 }
