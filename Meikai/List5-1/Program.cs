5a94071fdd7b4f5d402c617acc3d79bb16ea7812
kishimoto.mayumi <kishimoto.mayumi@toyoko-sys.co.jp>
2018-01-16 19:38:50 +0900

    階乗の計算

---
+++ Meikai/List5-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List5_1
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n > 0)
+            {
+                return n * factorial(n - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        static int Main(string[] args)
+        {
+            int x;
+            Console.Write("自然数を入力せよ：");
+            string stx = Console.ReadLine();
+            x = int.Parse(stx);
+
+            int result = factorial(x);
+            if (result == 1) {
+                Console.WriteLine("自然数ではありません。");
+            }
+            else
+            {
+                Console.WriteLine("{0}の階乗は{1}です。", x, result);
+            }
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
